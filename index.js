5dbba38c13ca4916d27e2682e5b81d42b81dd5da
Rakibulislam-emon <rakibulislamemon60>
2025-03-06 20:56:14 +0600

    brands added

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Function to load components
+async function loadComponent(componentId, filePath) {
+  const response = await fetch(filePath);
+  const content = await response.text();
+  document.getElementById(componentId).innerHTML = content;
+}
+
+// Load header and hero sections
+loadComponent("header-placeholder", "components/Navbar.html");
+loadComponent("hero-placeholder", "components/Hero.html");
+loadComponent("brands-placeholder", "components/Brands.html");
+
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
